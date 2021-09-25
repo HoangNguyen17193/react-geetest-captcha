5f7112e4348b163037cef7d949b9d4e0f0ffdd5d
Hoang Nguyen <hoangnh17193@gmail.com>
2021-09-25 13:08:29 +0700

    config jest for testing react component

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  verbose: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  verbose: true,
   globals: {
     'ts-jest': {
       diagnostics: false,
+      babelConfig: true,
+      tsconfig: 'tsconfig.json',
     },
     window: {},
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
 };
