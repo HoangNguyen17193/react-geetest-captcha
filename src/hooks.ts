087d2e41a13348594c4332ea87b7ab19e3301b62
Hoang Nguyen <hoangnh17193@gmail.com>
2021-09-25 12:36:14 +0700

    add types, config eslint for import

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,47 +1,7 @@
 import { useEffect, useState } from 'react';
 import { isFunction } from 'lodash';
 
-export interface RegisterData {
-    gt: string;
-    challenge: string;
-    new_captcha: boolean;
-    success: boolean;
-}
-
-export interface GeetestConfig {
-    type: 'popup' | 'bind' | 'float' | 'custom';
-    registerData: RegisterData;
-    appendTo?: string;
-    width?: string;
-    lang? :string
-    https?: boolean;
-    timeout?: number;
-    hideClose?: boolean;
-    hideSuccess?: boolean;
-    hideRefresh?: boolean;
-    onSuccess?: (callback: () => void) =>void;
-    onClose?: (callback: () => void) => void;
-    onReady?: (callback: () => void) => void;
-}
-
-export interface ValidateData {
-    geetest_challenge: string;
-    geetest_validate: string;
-    geetest_seccode: string;
-}
-
-export interface GeetestCaptcha {
-    onReady: (callback: () => void) => void
-    onSuccess: (callback: () => void) => void
-    onClose: (callback: () => void) => void
-    verify: () => void
-    destroy: () => void
-    reset: () => void
-    show: () => void
-    hide: () => void
-    getValidate: () => ValidateData
-    appendTo: (selector: string) => void
-}
+import { GeetestConfig, GeetestCaptcha } from './types';
 
 export const useGeetestCaptcha = (props: GeetestConfig) => {
   const {
